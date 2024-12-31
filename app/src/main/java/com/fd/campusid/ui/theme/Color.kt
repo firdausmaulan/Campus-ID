a58db387d8e2caed6725e04fa6ca23359459f60a
maulana.firdaus <maulana.firdaus@advotics.com>
2024-12-31 19:31:38 +0700

    Initial Commit - set color scheme - set project structure - add required dependencies / libraries

---
+++ app/src/main/java/com/fd/campusid/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.fd.campusid.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF10a8e4)
+val Secondary = Color(0xFFF4367A)
+val Tertiary = Color(0xFF10a8e4)
+val Background = Color(0xFFC6E8FF)
